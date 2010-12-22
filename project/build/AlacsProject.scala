19e0897073fb49ddf6e6c95f317b863ef30490c2
Yuvi Masory <ymasory@gmail.com>
2010-12-22 14:31:19 -0500

    use md extension

--- project/build/AlacsProject.scala
+++ project/build/AlacsProject.scala
@@ -5,7 +5,7 @@ class AlacsProject(info: ProjectInfo) extends DefaultProject(info) with Exec {
   val findBugs = "com.google.code.findbugs" % "findbugs" % "1.3.9"
 
   override def artifactID = "Alacs"
-  override def mainResources = super.mainResources +++ "README.markdown" +++ "LICENSE"
+  override def mainResources = super.mainResources +++ "README.md" +++ "LICENSE"
   override def compileOptions = super.compileOptions ++ Seq(Deprecation, Unchecked)
   override def documentOptions = super.documentOptions ++ Seq(LinkSource)
 }
