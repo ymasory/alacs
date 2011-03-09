b509ea425121e11e762dc4662fad43418afa85de
Yuvi Masory <ymasory@gmail.com>
2011-03-08 21:12:01 -0500

    cancel compilation after plugin runs to speed things up

--- src/main/scala/AlacsPlugin.scala
+++ src/main/scala/AlacsPlugin.scala
@@ -29,6 +29,7 @@ class AlacsPlugin(val global: Global) extends Plugin {
           curReport = analyzeTree(tree, curReport)
         }
         println("\n" + curReport.report)
+        global.currentRun.cancel
       }
     }
   }
