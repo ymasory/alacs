45b61eaa92653fbeb2f15c675450d12a6209799f
Yuvi Masory <ymasory@gmail.com>
2011-04-10 19:10:05 -0400

    remove magic constant

--- src/test/scala/AlacsTestSuite.scala
+++ src/test/scala/AlacsTestSuite.scala
@@ -4,12 +4,14 @@ import org.scalatest.FunSuite
 
 class AlacsFunSuite extends FunSuite {
 
+  val commonDir = "common/"
+
   def runAll(dirName: String, fileName: String): List[PluginMessage] = 
     RunPlugin.runPlugin(dirName + fileName + ".scala")
 
   def runBugs(dirName: String, fileName: String): List[Bug] = runAll(dirName, fileName)
 
-  def commonRunAll(fileName: String): List[PluginMessage] =  runAll("common/", fileName)
+  def commonRunAll(fileName: String): List[PluginMessage] =  runAll(commonDir, fileName)
 
-  def commonRunBugs(fileName: String): List[Bug] =  runBugs("common/", fileName)
+  def commonRunBugs(fileName: String): List[Bug] =  runBugs(commonDir, fileName)
 }
