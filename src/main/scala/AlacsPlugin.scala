0875ddc2657d2dea19368be2d8bfb7d0df448ff3
Yuvi Masory <ymasory@gmail.com>
2011-04-11 21:38:47 -0400

    remove old imports

--- src/main/scala/AlacsPlugin.scala
+++ src/main/scala/AlacsPlugin.scala
@@ -2,11 +2,7 @@ package com.github.alacs
 
 import scala.tools.nsc
 import nsc.Global
-import nsc.Phase
 import nsc.plugins.Plugin
-import nsc.plugins.PluginComponent
-
-import scala.collection.{mutable => m}
 
 class AlacsPlugin(val global: Global) extends Plugin {
   import global._
