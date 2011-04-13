0ec83b9f28c32c03110267c40e1300daaf223d44
Yuvi Masory <ymasory@gmail.com>
2011-04-13 19:16:55 -0400

    move out nested projects

--- project/build/AlacsMetaProject.scala
+++ project/build/AlacsMetaProject.scala
@@ -1,7 +1,21 @@
 import sbt._
 
-class AlacsMetaProject(info: ProjectInfo) extends DefaultProject(info) {
+class AlacsMetaProject(info: ProjectInfo) extends ParentProject(info) {
   
+  //turn down logging a little
+  log.setLevel(Level.Warn)
+  log.setTrace(2)
+
+  //the compiler plugin project
+  lazy val alacsProject = project("alacs", "Alacs Plugin", new AlacsProject(_))
+
+  //testing subprojects, one for each bug pattern
+  lazy val alacs001 = project("patterns" / "001-unintentional-procedure",
+                              "001 Unintentional Procedure",
+                              new AlacsPatternTestProject(_))
+}
+
+protected class AlacsProject(info: ProjectInfo) extends DefaultProject(info) {
   //project name
   override val artifactID = "alacs"
 
@@ -21,44 +35,28 @@ class AlacsMetaProject(info: ProjectInfo) extends DefaultProject(info) {
   val extraResources = "README.md" +++ "LICENSE"
   override val mainResources = super.mainResources +++ extraResources
 
-  //turn down logging a little
-  log.setLevel(Level.Warn)
-  log.setTrace(2)
+  //compiler options
+  override def compileOptions = super.compileOptions ++
+    compileOptions("-deprecation", "-unchecked")
 
-  //the compiler plugin project
-  lazy val alacsProject = project("alacs", "Alacs Plugin", new AlacsProject(_))
+  //scaladoc options
+  override def documentOptions =
+    LinkSource ::
+    documentTitle(name + " " + version + " API") ::
+    windowTitle(name + " " + version + " API") ::
+    Nil
+}
 
-  //testing subprojects, one for each bug pattern
-  lazy val alacs001 = project("patterns" / "001-unintentional-procedure",
-                              "001 Unintentional Procedure",
-                              new AlacsPatternTestProject(_))
+protected class AlacsPatternTestProject(info: ProjectInfo) 
+          extends DefaultProject(info)  with AutoCompilerPlugins {
 
-  class AlacsProject(info: ProjectInfo) extends DefaultProject(info) {
+  val scalatools = ("Scala Tools Nexus" at
+                    "http://nexus.scala-tools.org/" +
+                    "content/repositories/snapshots")
+  //managed dependencies from built-in repositories
+  val scalatest = "org.scalatest" % "scalatest" % "1.2"
 
-    //compiler options
-    override def compileOptions = super.compileOptions ++
-      compileOptions("-deprecation", "-unchecked")
-
-    //scaladoc options
-    override def documentOptions =
-      LinkSource ::
-      documentTitle(name + " " + version + " API") ::
-      windowTitle(name + " " + version + " API") ::
-      Nil
-  }
-
-
-  class AlacsPatternTestProject(info: ProjectInfo) 
-    extends DefaultProject(info)  with AutoCompilerPlugins {
-
-      val scalatools = ("Scala Tools Nexus" at
-                        "http://nexus.scala-tools.org/" +
-                        "content/repositories/snapshots")
-      //managed dependencies from built-in repositories
-      val scalatest = "org.scalatest" % "scalatest" % "1.2"
-
-      //compiler options
-      override def compileOptions = super.compileOptions ++
-        compileOptions("-deprecation", "-unchecked")    
-  }
+  //compiler options
+  override def compileOptions = super.compileOptions ++
+    compileOptions("-deprecation", "-unchecked")    
 }
