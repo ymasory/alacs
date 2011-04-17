7ad1d8fb11fa3fc780e174ea09524d2faf91d577
Yuvi Masory <ymasory@gmail.com>
2011-04-17 09:58:05 -0400

    use ijuma's version string technique

--- src/test/scala/RunPlugin.scala
+++ src/test/scala/RunPlugin.scala
@@ -30,10 +30,8 @@ object RunPlugin {
 
   def getRunningScalaVersion() = {
     try {
-      val props = new java.util.Properties
-      props.load(getClass.getResourceAsStream("/library.properties"))
-      val line = props.getProperty("version.number")
-      val Version = """(\d\.\d\.\d).*""".r
+      val line = util.Properties.versionString
+      val Version = """version (\d\.\d\.\d).*""".r
       val Version(versionStr) = line
       versionStr
     }
